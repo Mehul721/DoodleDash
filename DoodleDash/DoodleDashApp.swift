b21b5daf08e461bb376b57127539fbb13378ac8b
Mehul721 <jmehul721@gmail.com>
2024-08-11 22:57:27 +0530

    Initial Commit

---
+++ DoodleDash/DoodleDashApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoodleDashApp.swift
+//  DoodleDash
+//
+//  Created by Mehul Jain on 11/08/24.
+//
+
+import SwiftUI
+
+@main
+struct DoodleDashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
